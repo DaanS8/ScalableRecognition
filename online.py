f044782fbd25c330d873c44efd796c89ffed03d1
DaanS8 <72094257+DaanS8@users.noreply.github.com>
2021-09-13 15:44:21 +0200

    Online

--- online.py
+++ online.py
@@ -4,7 +4,7 @@ import cv2 as cv
 
 def main():
     image_path = ""
-    while not os.path.isfile(image_path)
+    while not os.path.isfile(image_path):
         image_path = input("Give path to input image (jpg):")
         if image_path[-4:] != ".jpg":
             image_path += ".jpg"
